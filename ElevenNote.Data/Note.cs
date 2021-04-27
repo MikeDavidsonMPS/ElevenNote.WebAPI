50d246aa06c2a8a28246aeab2d533e54adc337fe
michael Davidson <meridianproservice@outlook.com>
2021-04-27 18:56:25 -0400

    new directory

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -10,7 +10,7 @@ namespace ElevenNote.Data
     public class Note
     {
         [Key]
-        public int NoteId { get; set; }
+        public int NoteId { get; set; }  
         [Required]
         public Guid OwnerId { get; set; }
         [Required]
